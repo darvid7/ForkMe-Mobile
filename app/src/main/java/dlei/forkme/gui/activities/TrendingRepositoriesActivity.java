707ba42d5296aeca9d7d9081a7da0c42c524b0b3
darvid7 <bvdarvid@gmail.com>
2017-04-24 18:13:13 +1000

    app init

---
+++ app/src/main/java/dlei/forkme/gui/activities/TrendingRepositoriesActivity.java
@@ -0,0 +1,16 @@
+package dlei.forkme.gui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dlei.forkme.R;
+
+public class TrendingRepositoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trending_repositories);
+
+    }
+}
